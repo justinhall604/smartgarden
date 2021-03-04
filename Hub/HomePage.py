bdbaf839ccaf493b8964238512caf07da87e0bc0
kirstenmarieh <kirstmarieh@gmail.com>
2021-03-04 13:25:00 -0500

    code actually updated (graphs implemented, decode UUID, write to file)

--- Hub/HomePage.py
+++ Hub/HomePage.py
@@ -1,19 +1,8 @@
-import kivy
-import time
-import bluetooth
+from adafruit_ble.advertising import to_bytes_literal
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-import BLE_GATT
-from beacontools import parse_packet
-import struct
-import kivy
-import bluetooth
-from kivy.app import App
-from kivy.lang import Builder
+from kivy.clock import Clock
 import asyncio
-from bleak import BleakScanner
-from beacontools import parse_packet
 from kivy.uix.gridlayout import Layout
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
@@ -24,10 +13,13 @@ from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.screenmanager import ScreenManager, Screen
 import time
 from adafruit_ble import BLERadio
-import datetime
+import binascii
+from kivy_garden.graph import Graph, MeshLinePlot
 
 Builder.load_string("""
 <HomeScreen>:
+        on_enter:
+                root.start()
         FloatLayout:
                 Label:   
                         id: HSlabel
@@ -54,13 +46,13 @@ Builder.load_string("""
         on_pre_enter:
                 root.pre_enter()
                 
-                Label: 'Connecting to device...'
-                
         on_enter:
                 root.on_entered()
         
         FloatLayout:
-
+                Label:
+                        size: self.texture_size
+                        text_size: cm(6), cm(4)
                 Button:
                         id: home
                         size_hint: .1, .1
@@ -73,74 +65,317 @@ Builder.load_string("""
         on_enter:
                 root.on_enter()
         FloatLayout:
+                Label:
+                        text: 'Historical Readings'
+                        pos_hint: {"x":.0, "y":.45}
+                        font_size: 30
                 Button:
                         id: home
                         size_hint: .1, .1
                         text: 'Home'
                         on_press: root.manager.current = 'Home'
                         pos_hint: {"x":.01, "top":1}
+    
 """)
 
 
 class HomeScreen(Screen):
-    pass
+    def getAndWrite(self, dt):
+        ble = bletooth()
+        ble.scanForBle()
+        self.uuid = ble.getAdvertisement()
+        self.decode_data()
+        self.writeToFile()
+
+    def start(self):
+        Clock.schedule_interval(self.getAndWrite, 600)
+
+    def decode_data(self):
+        uuidDecoded = self.uuid.decode()
+        uuid = []
+        for i in range(len(uuidDecoded)):
+            try:
+                int(uuidDecoded[i])
+                uuid.append(uuidDecoded[i])
+
+            except:
+                if uuidDecoded[i] == 'a':
+                    uuid.append(10)
+                elif uuidDecoded[i] == 'b':
+                    uuid.append(11)
+                elif uuidDecoded[i] == 'c':
+                    uuid.append(12)
+                elif uuidDecoded == 'd':
+                    uuid.append(13)
+                elif uuidDecoded == 'e':
+                    uuid.append(14)
+                else:
+                    uuid.append(15)
+
+        self.outsideTemp = int(uuid[0])*16 + int(uuid[1])
+        self.humidity = int(uuid[2])*16+int(uuid[3])
+        self.moistureLevel = int(uuid[4])*16+int(uuid[5])
+        self.battVoltage = int(uuid[6])*16+int(uuid[7])
+        self.lightLevel = int(uuid[8])*16+int(uuid[9])
+        self.picoTemp = int(uuid[10])*16+int(uuid[11])
+        self.rainEvents = int(uuid[12])*16+int(uuid[13])
+
+    def writeToFile(self):
+        readingsFile = open("HistoricalReadings.txt", 'a')
+        readTime = time.asctime()
+        readingsFile.write(readTime + "\t")
+        readingsFile.write(str(self.outsideTemp) + "\t\t\t\t\t\t\t")  # 'Outside Temperature: ' +
+        readingsFile.write(str(self.humidity) + "\t\t\t\t")  # 'Humidity:  ' +
+        readingsFile.write(str(self.moistureLevel) + "\t\t\t\t\t\t\t")  # 'Soil Moisture Level: ' +
+        readingsFile.write(str(self.battVoltage) + "\t\t\t\t\t")  # 'Battery Voltage: ' +
+        readingsFile.write(str(self.lightLevel) + "\t\t\t\t\t")  # 'Light Level: ' +
+        readingsFile.write(str(self.picoTemp) + "\t\t\t\t\t\t")  # 'PICO Temperature: ' +
+        readingsFile.write(str(self.rainEvents) + "\n")  # 'Rain Events: ' +
+        readingsFile.close()
+
+
+from kivy.uix.modalview import ModalView
+class HistoryReadingsScreen(Screen):
+
+    def on_enter(self):
+        self.read_file()
+        #box = BoxLayout(orientation= "horizontal", padding = [0,15,15,10])
+        Graphs = [self.tempGraph(), self.humidGraph(), self.moistureGraph(), self.lightGraph(), self.batteryGraph(), self.picoGraph(), self.rainGraph()]
+        for i in range(len(Graphs)):
+            view = ModalView(size_hint=(0.5,0.5))
+            graph = Graphs[i]
+            view.add_widget(graph)
+            #view.open()
+            self.add_widget(view)
+
+    def read_file(self):
+        self.historicalData = open("HistoricalReadings.txt", 'r')
+        self.fileData = []
+
+        with open("HistoricalReadings.txt") as f:
+            for line in f:
+                self.fileData.append(line.split())
+
+        self.historicalData.close()
+
+    def tempGraph(self):
+
+        tempY =[] ##graph y values, all data self.tempY
+
+        for i in range(1, len(self.fileData)):
+            tempY.append(self.fileData[i][5])
+
+
+        fileLength = len(self.fileData)
+        plot = None
+        graph = Graph(ylabel='Outside Temperature', xlabel = 'Time', x_ticks_major = 1, y_ticks_minor = 1, y_ticks_major = 1,
+                  y_grid_label=True, x_grid_label=True, padding=5, x_grid=True, y_grid=True,
+                  xmin=0, xmax=50, ymin=0, ymax=200)
+
+        plot = MeshLinePlot(color = [1,2,3,4])
+        print(len(self.fileData))
+        print(tempY)
+        print(len(tempY))
+        plot.points = [(int(i), int(tempY[i])) for i in range(1, len(self.fileData)-1)]
+        graph.add_plot(plot)
+        return graph
+
+    def humidGraph(self):
+        humidY = []
+
+        for i in range(1, len(self.fileData)):
+            humidY.append(self.fileData[i][6])
+
+        humidityPlot = None
+        humidityGraph = Graph(ylabel='Humidity', xlabel='Time', x_ticks_major=1, y_ticks_minor=1, y_ticks_major=1,
+                      y_grid_label=True, x_grid_label=True, padding=5, x_grid=True, y_grid=True,
+                      xmin=0, xmax=50, ymin=0, ymax=200)
+
+        humidityPlot = MeshLinePlot(color=[1, 2, 3, 4])
+        humidityPlot.points = [(int(i), int(humidY[i])) for i in range(1, len(self.fileData) - 1)]
+        humidityGraph.add_plot(humidityPlot)
+
+        return humidityGraph
+
+    def moistureGraph(self):
+        moistY = []
+        for i in range(1, len(self.fileData)):
+            moistY.append(self.fileData[i][7])
+
+        plot = None
+        graph = Graph(ylabel='Moisture Level', xlabel='Time', x_ticks_major=1, y_ticks_minor=1,
+                              y_ticks_major=1,
+                              y_grid_label=True, x_grid_label=True, padding=5, x_grid=True, y_grid=True,
+                              xmin=0, xmax=50, ymin=0, ymax=200)
+
+        plot = MeshLinePlot(color=[1, 2, 3, 4])
+        plot.points = [(int(i), int(moistY[i])) for i in range(1, len(self.fileData) - 1)]
+        graph.add_plot(plot)
+
+        return graph
+
+    def batteryGraph(self):
+        battY = []
+        for i in range(1, len(self.fileData)):
+            battY.append(self.fileData[i][8])
+
+        plot = None
+        graph = Graph(ylabel='Battery Level', xlabel='Time', x_ticks_major=1, y_ticks_minor=1,
+                      y_ticks_major=1,
+                      y_grid_label=True, x_grid_label=True, padding=5, x_grid=True, y_grid=True,
+                      xmin=0, xmax=50, ymin=0, ymax=200)
+
+        plot = MeshLinePlot(color=[1, 2, 3, 4])
+        plot.points = [(int(i), int(battY[i])) for i in range(1, len(self.fileData) - 1)]
+        graph.add_plot(plot)
+
+        return graph
+
+    def lightGraph(self):
+        lightY = []
+        for i in range(1, len(self.fileData)):
+            lightY.append(self.fileData[i][9])
+
+
+        plot = None
+        graph = Graph(ylabel='Light Level', xlabel='Time', x_ticks_major=1, y_ticks_minor=1,
+                      y_ticks_major=1,
+                      y_grid_label=True, x_grid_label=True, padding=5, x_grid=True, y_grid=True,
+                      xmin=0, xmax=50, ymin=0, ymax=200)
+
+        plot = MeshLinePlot(color=[1, 2, 3, 4])
+        plot.points = [(int(i), int(lightY[i])) for i in range(1, len(self.fileData) - 1)]
+        graph.add_plot(plot)
+
+        return graph
+
+    def picoGraph(self):
+        picoY = []
+        for i in range(1, len(self.fileData)):
+            picoY.append(self.fileData[i][10])
+
+        plot = None
+        graph = Graph(ylabel='Pico Temperature', xlabel='Time', x_ticks_major=1, y_ticks_minor=1,
+                      y_ticks_major=1,
+                      y_grid_label=True, x_grid_label=True, padding=5, x_grid=True, y_grid=True,
+                      xmin=0, xmax=50, ymin=0, ymax=200)
+
+        plot = MeshLinePlot(color=[1, 2, 3, 4])
+        plot.points = [(int(i), int(picoY[i])) for i in range(1, len(self.fileData) - 1)]
+        graph.add_plot(plot)
+
+        return graph
+
+    def rainGraph(self):
+        rainY = []
+        for i in range(1, len(self.fileData)):
+            rainY.append(self.fileData[i][11])
+        plot = None
+        graph = Graph(ylabel='Rain Events', xlabel='Time', x_ticks_major=1, y_ticks_minor=1,
+                      y_ticks_major=1,
+                      y_grid_label=True, x_grid_label=True, padding=5, x_grid=True, y_grid=True,
+                      xmin=0, xmax=50, ymin=0, ymax=200)
+
+        plot = MeshLinePlot(color=[1, 2, 3, 4])
+        plot.points = [(int(i), int(rainY[i])) for i in range(1, len(self.fileData) - 1)]
+        graph.add_plot(plot)
+
+        return graph
+
 
 class CurrentReadingsScreen(Screen):
     def pre_enter(self):
-        #print('Connecting to device')
-        #self.beacon = blueTooth(BLE_GATT.Central('AC:BC:32:AC:F8:B4'), '00002222-0000-1000-8000-00805F9B34FB')
+        ble = bletooth()
+        ble.scanForBle()
+        self.uuid = ble.getAdvertisement()
+
+
+    def on_entered(self):
+        print("scan done")
+        self.decode_data()
+
+        #self.writeToFile()
+        ##display information
+
+    def decode_data(self):
+        uuidDecoded = self.uuid.decode()
+        uuid = []
+        for i in range(len(uuidDecoded)):
+            try:
+                int(uuidDecoded[i])
+                uuid.append(uuidDecoded[i])
+
+            except:
+                if uuidDecoded[i] == 'a':
+                    uuid.append(10)
+                elif uuidDecoded[i] == 'b':
+                    uuid.append(11)
+                elif uuidDecoded[i] == 'c':
+                    uuid.append(12)
+                elif uuidDecoded == 'd':
+                    uuid.append(13)
+                elif uuidDecoded == 'e':
+                    uuid.append(14)
+                else:
+                    uuid.append(15)
+
+        self.outsideTemp = int(uuid[0])*16 + int(uuid[1])
+        self.humidity = int(uuid[2])*16+int(uuid[3])
+        self.moistureLevel = int(uuid[4])*16+int(uuid[5])
+        self.battVoltage = int(uuid[6])*16+int(uuid[7])
+        self.lightLevel = int(uuid[8])*16+int(uuid[9])
+        self.picoTemp = int(uuid[10])*16+int(uuid[11])
+        self.rainEvents = int(uuid[12])*16+int(uuid[13])
+
+    def writeToFile(self):
+        readingsFile = open("HistoricalReadings.txt", 'a')
+        readTime = time.asctime()
+        readingsFile.write(readTime + "\t")
+        readingsFile.write(str(self.outsideTemp) + "\t\t\t\t\t\t\t")#'Outside Temperature: ' +
+        readingsFile.write(str(self.humidity) + "\t\t\t\t")#'Humidity:  ' +
+        readingsFile.write(str(self.moistureLevel) + "\t\t\t\t\t\t\t")#'Soil Moisture Level: ' +
+        readingsFile.write(str(self.battVoltage) + "\t\t\t\t\t")#'Battery Voltage: ' +
+        readingsFile.write(str(self.lightLevel) + "\t\t\t\t\t")#'Light Level: ' +
+        readingsFile.write(str(self.picoTemp) + "\t\t\t\t\t\t")#'PICO Temperature: ' +
+        readingsFile.write(str(self.rainEvents) + "\n")#'Rain Events: ' +
+        readingsFile.close()
+
+
+class bletooth():
+    def scanForBle(self):
         ble = BLERadio()
         print("scanning")
         found = set()
+
+        self.byteList = []
         scan_responses = set()
         for advertisement in ble.start_scan():
+            self.byteList.append(bytes(advertisement))
             addr = advertisement.address
             if advertisement.scan_response and addr not in scan_responses:
                 scan_responses.add(addr)
             elif not advertisement.scan_response and addr not in found:
                 found.add(addr)
+                found.add(advertisement)
             else:
                 continue
-            print(addr, advertisement)
-            print("\t" + repr(advertisement))
             print()
+            ble.stop_scan()
 
-        print("scan done")
-
-    def on_entered(self):
-
-
-        print("scan done")
-        #self.beacon.readFromDevice()
-        #self.beacon.parseData()
-        #self.writeToFile()
-
-    def writeToFile(self):
-        readingsFile = open("HistoricalReadings.txt", 'a')
-        readTime = time.asctime()
-        #PARSE AND GET INFO
-
-        readingsFile.write(readTime + " ")
-        readingsFile.write('Temperature ')
-        readingsFile.write('pH "')
-        readingsFile.write('Water Level ')
-        readingsFile.write('Humidity ')
-        readingsFile.write('\n\n\n\n')
-
-        readingsFile.close()
-
-
-
-class HistoryReadingsScreen(Screen):
-    def on_enter(self):
-        # self.open_file()
-        self.readFile()
-
-    def readFile(self):
-        historicalData = open("HistoricalReadings.txt", 'r')
-        historicalData.read()
-        print(historicalData.read())
-
+    def getAdvertisement(self):
+        for adBytes in self.byteList:
+            #print(len(adBytes))
+            print()
+            print()
+            if len(adBytes) == 39:  ##length of nrfBlinky advertisement
+                self.targetBytes = binascii.b2a_hex(adBytes)
+                self.targetButes = to_bytes_literal(adBytes)
+                print("target: ", self.targetButes)
+                break
+        print("target: ", self.targetBytes)
+        bArray = bytearray(self.targetBytes)
+        self.uuid = bArray[12:44]
+        return self.uuid
 
 class TestApp(App):
 
@@ -151,68 +386,10 @@ class TestApp(App):
         sm.add_widget(HistoryReadingsScreen(name='History'))
         return sm
 
-class blueTooth(object):
-    def __init__(self, my_device, my_uuid):
-        self.my_device = my_device
-        self.my_uuid = my_uuid
-
-    def readFromDevice(self):
-        self.my_device.connect()
-        self.value = self.my_device.char_read(self.my_uuid)
-        print(self.value)
-        #print(type(self.value))
-        self.my_device.disconnect()
-
-    #def parseData(self):
-        #print(int.from_bytes(self.value[0:2], byteorder='little', signed=False))
-
-
 
 if __name__ == "__main__":
+
+    #timer = TimeApp()
+   # timer.run()
     HomePage = TestApp()
     HomePage.run()
-
-
-
-#if __name__ == "__main__":
-
-    #run()
-       # bt = blueTooth(BLE_GATT.Central('AC:BC:32:AC:F8:B4'), '00002222-0000-1000-8000-00805F9B34FB')
-       #  bt.readFromDevice()
-        #bt.parseData()
-
-       #  my_device = BLE_GATT.Central('AC:BC:32:AC:F8:B4')
-       #  my_uuid = '00002222-0000-1000-8000-00805F9B34FB'
-       #  my_device.connect()
-       #  value = my_device.char_read(my_uuid)
-       #  print(value)
-       # # print(type(value))
-       #  my_device.disconnect()
-# async def run():
-#     devices = await BleakScanner.discover()
-#     for d in devices:
-#         print(d)
-
-#loop = asyncio.get_event_loop()
-#loop.run_until_complete(run())
-
-
-
-ble = BLERadio()
-print("scanning")
-found = set()
-scan_responses = set()
-for advertisement in ble.start_scan():
-    addr = advertisement.address
-    if advertisement.scan_response and addr not in scan_responses:
-        scan_responses.add(addr)
-    elif not advertisement.scan_response and addr not in found:
-        found.add(addr)
-    else:
-        continue
-    print(addr, advertisement)
-    print("\t" + repr(advertisement))
-    print()
-
-print("scan done")
-
